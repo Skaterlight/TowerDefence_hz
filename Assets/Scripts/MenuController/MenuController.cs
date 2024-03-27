4d6c4f7770c361e1039efc6eaf2cb4b3acb48ab5
Vect0r555 <164627030+Vect0r555@users.noreply.github.com>
2024-03-27 19:11:45 +0200

    MenuCreationStart
    почав створювати меню,створив нову сцену спеціально під меню. Зробив головну панель та трохи коду, також завантажив декілька UI ассетів.

---
+++ Assets/Scripts/MenuController/MenuController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject MainMenuPanel;
+    public GameObject settingsMenuPanel;
+    public GameObject levelsMenuPanel;
+    private void Start()
+    {
+        MainMenuPanel.SetActive(true);
+        settingsMenuPanel.SetActive(false);
+        levelsMenuPanel.SetActive(false);
+    }
+    public void ChooseLevel()
+    {
+        MainMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(false);
+        levelsMenuPanel.SetActive(true);
+    }
+    public void OpenSettings()
+    {
+        MainMenuPanel.SetActive(false);
+        levelsMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(true);
+    }
+}
