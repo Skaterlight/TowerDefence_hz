895f2042b9043265b88440bfef5438ee5c667f3c
Vect0r555 <164627030+Vect0r555@users.noreply.github.com>
2024-03-28 18:48:42 +0200

    MenuCreationContinues
    додав білше функцій у меню, скачав ще ассетів.

--- Assets/Scripts/MenuController/MenuController.cs
+++ Assets/Scripts/MenuController/MenuController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class MenuController : MonoBehaviour
 {
@@ -9,6 +12,10 @@ public class MenuController : MonoBehaviour
     public GameObject settingsMenuPanel;
     public GameObject levelsMenuPanel;
     private void Start()
+    {
+        LoadMainMenu();
+    }
+    public void LoadMainMenu()
     {
         MainMenuPanel.SetActive(true);
         settingsMenuPanel.SetActive(false);
@@ -25,5 +32,13 @@ public class MenuController : MonoBehaviour
         MainMenuPanel.SetActive(false);
         levelsMenuPanel.SetActive(false);
         settingsMenuPanel.SetActive(true);
+    }
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
