aed06f217b58735dee0c9837dd3627d32eb886fc
Vect0r555 <164627030+Vect0r555@users.noreply.github.com>
2024-04-06 21:58:22 +0300

    Level1UITestScene

--- Assets/Scripts/MenuController/LevelFirstUIController.cs
+++ Assets/Scripts/MenuController/LevelFirstUIController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelFirstUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider slider;
+    public AudioSource audioSource;
+    public GameObject Panel;
+    private bool soundIsActive;
     void Start()
     {
-        
+        Panel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        { 
+          Panel.SetActive(true); 
+        }
+        audioSource.volume = slider.value;
     }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
 }
