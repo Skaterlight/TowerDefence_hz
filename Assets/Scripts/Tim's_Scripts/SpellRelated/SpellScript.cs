1f78771ece93aa68a98acfe1516d7e853ba85eb2
Skaterlight <164627141+Skaterlight@users.noreply.github.com>
2024-05-09 19:02:47 +0300

    FixedTowers
    looks worse but works

--- Assets/Scripts/Tim's_Scripts/SpellRelated/SpellScript.cs
+++ Assets/Scripts/Tim's_Scripts/SpellRelated/SpellScript.cs
@@ -7,4 +7,9 @@ public class SpellScript : MonoBehaviour
     [SerializeField] private SpellStats stats;
     public int Damage => stats.Damage;
     public int Lasting => stats.Lasting;
+
+    private void Start()
+    {
+        Destroy(gameObject, 1f);
+    }
 }
